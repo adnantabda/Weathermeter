64270d44be3abc7c3f9161fe3976e259e655518c
Adnan Tahir <adnantabda@gmail.com>
2024-05-30 20:32:58 +0300

    Add functions to update weather sequences on the DOM
    - Added  to update specific time, icon, and temperature for weather sequence 1.
    - Added  to update specific time, icon, and temperature for weather sequence 2.
    - Added  to update specific time, icon, and temperature for weather sequence 3.
    - Added  to update specific time, icon, and temperature for weather sequence 4.
    - Added  to update specific time, icon, and temperature for weather sequence 5.
    - Added  to update specific time, icon, and temperature for weather sequence 6.
    - Added  to update specific time, icon, and temperature for weather sequence 7.

---
+++ src/script/dom-builder.js
@@ -0,0 +1,87 @@
+export function weatherSequenceDom_1(specificTime, iconLink , temp){
+
+    let time = document.querySelector(".weather-sequence_1 .specific-time")
+    let icon = document.querySelector(".weather-sequence_1 img")
+    let temperature = document.querySelector(".weather-sequence_1 .specific-temperature")
+
+    time.textContent = specificTime
+    icon.src = iconLink
+    temperature.innerHTML = `${temp}&deg;C`
+
+}
+
+
+export function weatherSequenceDom_2(specificTime, iconLink , temp){
+    
+    let time = document.querySelector(".weather-sequence_2 .specific-time")
+    let icon = document.querySelector(".weather-sequence_2 img")
+    let temperature = document.querySelector(".weather-sequence_2 .specific-temperature")
+
+    time.textContent = specificTime
+    icon.src = iconLink
+    temperature.innerHTML = `${temp}&deg;C`
+
+}
+
+
+export function weatherSequenceDom_3(specificTime, iconLink , temp){
+    
+    let time = document.querySelector(".weather-sequence_3 .specific-time")
+    let icon = document.querySelector(".weather-sequence_3 img")
+    let temperature = document.querySelector(".weather-sequence_3 .specific-temperature")
+
+    time.textContent = specificTime
+    icon.src = iconLink
+    temperature.innerHTML = `${temp}&deg;C`
+
+}
+
+
+export function weatherSequenceDom_4(specificTime, iconLink , temp){
+    
+    let time = document.querySelector(".weather-sequence_4 .specific-time")
+    let icon = document.querySelector(".weather-sequence_4 img")
+    let temperature = document.querySelector(".weather-sequence_4 .specific-temperature")
+
+    time.textContent = specificTime
+    icon.src = iconLink
+    temperature.innerHTML = `${temp}&deg;C`
+
+}
+
+
+export function weatherSequenceDom_5(specificTime, iconLink , temp){
+    
+    let time = document.querySelector(".weather-sequence_5 .specific-time")
+    let icon = document.querySelector(".weather-sequence_5 img")
+    let temperature = document.querySelector(".weather-sequence_5 .specific-temperature")
+
+    time.textContent = specificTime
+    icon.src = iconLink
+    temperature.innerHTML = `${temp}&deg;C`
+
+}
+
+
+export function weatherSequenceDom_6(specificTime, iconLink , temp){
+    
+    let time = document.querySelector(".weather-sequence_6 .specific-time")
+    let icon = document.querySelector(".weather-sequence_6 img")
+    let temperature = document.querySelector(".weather-sequence_6 .specific-temperature")
+
+    time.textContent = specificTime
+    icon.src = iconLink
+    temperature.innerHTML = `${temp}&deg;C`
+
+}
+
+export function weatherSequenceDom_7(specificTime, iconLink , temp){
+    
+    let time = document.querySelector(".weather-sequence_7 .specific-time")
+    let icon = document.querySelector(".weather-sequence_7 img")
+    let temperature = document.querySelector(".weather-sequence_7 .specific-temperature")
+
+    time.textContent = specificTime
+    icon.src = iconLink
+    temperature.innerHTML = `${temp}&deg;C`
+}
