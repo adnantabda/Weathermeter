5a5d2a9466df47e390ec704a4acca081de94f1d9
Adnan Tahir <adnantabda@gmail.com>
2024-05-31 21:23:48 +0300

    Refactor and Add: Map

--- src/script/main.js
+++ src/script/main.js
@@ -1,7 +1,26 @@
 import "../style/main.scss"
 import { weatherSequenceDom_1,weatherSequenceDom_2,weatherSequenceDom_3,weatherSequenceDom_4,weatherSequenceDom_5,weatherSequenceDom_6,weatherSequenceDom_7 } from "./dom-builder"
+import { getNearbyCities } from "./dom-builder";
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
 
 
+new Map({
+    target: 'map',
+    layers: [
+      new TileLayer({
+        source: new OSM(),
+      }),
+    ],
+    view: new View({
+      center: [0, 0],
+      zoom: 2,
+    }),
+  });
+
+  "http://api.openweathermap.org/data/2.5/find?lat={lat}&lon={lon}&cnt={cnt}&appid={API_KEY}"
 
 
 let weatherIcon = document.querySelector(".today-weather img")
@@ -11,9 +30,8 @@ const todayWeatherTitle = document.querySelector(".today-weather-title")
 const cityDisplay = document.querySelector(".city-name")
 const dateTime = document.querySelector(".date-time")
 let temperature = document.querySelector(".temperature")
-let emoji = document.querySelector(".emoji")
 
-function convertToCelsius(kelvin){
+export function convertToCelsius(kelvin){
     let result = kelvin - 273.15
     return result.toFixed(1)
 
@@ -39,76 +57,84 @@ function todayHighlight(weatherDescription, weatherHumidity, weatherPressure, we
 
 
 
-searchIcon.addEventListener("click" , ()=>{
-
-    const apiKey = "023ddb3cfa25160b6c95e5c0c0995ea0"
-    let cityName = input.value
-
-    async function getLocation(cityName, apiKey){
-        const latLongApi= `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`
-        const response = await fetch(latLongApi , {mode:'cors'})
-        const data = await response.json()
-        return  [data[0]["lat"], data[0]["lon"]]
-    }
-
-    async function getWeather(cityName, apiKey){
-        const location = await getLocation(cityName, apiKey)
-        console.log(location['lon'])
-        const weatherApi = `https://api.openweathermap.org/data/2.5/forecast?lat=${location[0]}&lon=${location[1]}&appid=023ddb3cfa25160b6c95e5c0c0995ea0&cnt=8`
-        const response = await fetch(weatherApi, {mode: 'cors'})
-        const data = await response.json()
-        console.log(data)
-        console.log(data)
-        let kelvin = data['list'][0]['main']['temp']
-        temperature.innerHTML = `${convertToCelsius(kelvin)}&deg;C`
-        let icon = data['list'][0]['weather'][0]['icon']
-        weatherIcon.src = `https://openweathermap.org/img/wn/${icon}.png`
-
-        let weatherTitle = data['list'][0]["weather"][0]["main"]
-        todayWeatherTitle.textContent = weatherTitle
-        let city__ = `${cityName}, ${data["city"]["country"]}`
-        cityDisplay.textContent = city__
-        dateTime.textContent = data["list"][0]["dt_txt"]
-
-        let weatherDescription = data['list'][0]["weather"][0]["description"]
-        let weatherPressure = data["list"][0]['main']["pressure"]
-        let weatherHumidity = data["list"][0]["main"]["humidity"]
-        let weatherSealevel = data["list"][0]["main"]["sea_level"]
-        let weatherSunrise = data["city"]["sunrise"]
-        let weatherSunset = data["city"]["sunset"]
-
-        todayHighlight(weatherDescription, weatherHumidity,weatherPressure , weatherSealevel, weatherSunset, weatherSunrise)
-
-        for (let i = 1 ; i <= 7 ; i++){
-            let tempK = data["list"][i]["main"]["temp"]
-            let tempC = convertToCelsius(tempK)
-
-            let specificWeatherIcon = data["list"][i]["weather"][0]['icon']
-            let specificWeatherIconLink = `https://openweathermap.org/img/wn/${specificWeatherIcon}.png`
-        
+async function getLocation(cityName, apiKey){
+    const latLongApi= `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`
+    const response = await fetch(latLongApi , {mode:'cors'})
+    const data = await response.json()
+    return  [data[0]["lat"], data[0]["lon"]]
+}
 
 
-            let specificTime = data["list"][i]["dt_txt"]
-            let specificTimeSliced = specificTime.slice(11,16)
-        
-            if (i === 1){
-                weatherSequenceDom_1(specificTimeSliced, specificWeatherIconLink, tempC)
-            }else if(i === 2){
-                weatherSequenceDom_2(specificTimeSliced, specificWeatherIconLink, tempC)
-            }else if(i == 3){
-                weatherSequenceDom_3(specificTimeSliced, specificWeatherIconLink, tempC)
-            }else if(i === 4){
-                weatherSequenceDom_4(specificTimeSliced, specificWeatherIconLink, tempC)
-            }else if(i === 5){
-                weatherSequenceDom_5(specificTimeSliced, specificWeatherIconLink, tempC)
-            }else if(i === 6){
-                weatherSequenceDom_6(specificTimeSliced, specificWeatherIconLink, tempC)
-            }else if(i === 7){
-                console.log(tempC)
-                weatherSequenceDom_7(specificTimeSliced, specificWeatherIconLink, tempC)
-            }
+async function getWeather(cityName, apiKey){
+    const location = await getLocation(cityName, apiKey)
+   
+
+    getNearbyCities(apiKey,location[0], location[1], cityName)
+
+
+
+    const weatherApi = `https://api.openweathermap.org/data/2.5/forecast?lat=${location[0]}&lon=${location[1]}&appid=023ddb3cfa25160b6c95e5c0c0995ea0&cnt=8`
+    const response = await fetch(weatherApi, {mode: 'cors'})
+    const data = await response.json()
+    let kelvin = data['list'][0]['main']['temp']
+    temperature.innerHTML = `${convertToCelsius(kelvin)}&deg;C`
+    let icon = data['list'][0]['weather'][0]['icon']
+    weatherIcon.src = `https://openweathermap.org/img/wn/${icon}.png`
+
+    let weatherTitle = data['list'][0]["weather"][0]["main"]
+    todayWeatherTitle.textContent = weatherTitle
+    let city__ = `${cityName}, ${data["city"]["country"]}`
+    cityDisplay.textContent = city__
+    dateTime.textContent = data["list"][0]["dt_txt"]
+
+    let weatherDescription = data['list'][0]["weather"][0]["description"]
+    let weatherPressure = data["list"][0]['main']["pressure"]
+    let weatherHumidity = data["list"][0]["main"]["humidity"]
+    let weatherSealevel = data["list"][0]["main"]["sea_level"]
+    let weatherSunrise = data["city"]["sunrise"]
+    let weatherSunset = data["city"]["sunset"]
+
+    todayHighlight(weatherDescription, weatherHumidity,weatherPressure , weatherSealevel, weatherSunset, weatherSunrise)
+
+    for (let i = 1 ; i <= 7 ; i++){
+        let tempK = data["list"][i]["main"]["temp"]
+        let tempC = convertToCelsius(tempK)
+
+        let specificWeatherIcon = data["list"][i]["weather"][0]['icon']
+        let specificWeatherIconLink = `https://openweathermap.org/img/wn/${specificWeatherIcon}.png`
+    
+
+
+        let specificTime = data["list"][i]["dt_txt"]
+        let specificTimeSliced = specificTime.slice(11,16)
+    
+        if (i === 1){
+            weatherSequenceDom_1(specificTimeSliced, specificWeatherIconLink, tempC)
+        }else if(i === 2){
+            weatherSequenceDom_2(specificTimeSliced, specificWeatherIconLink, tempC)
+        }else if(i == 3){
+            weatherSequenceDom_3(specificTimeSliced, specificWeatherIconLink, tempC)
+        }else if(i === 4){
+            weatherSequenceDom_4(specificTimeSliced, specificWeatherIconLink, tempC)
+        }else if(i === 5){
+            weatherSequenceDom_5(specificTimeSliced, specificWeatherIconLink, tempC)
+        }else if(i === 6){
+            weatherSequenceDom_6(specificTimeSliced, specificWeatherIconLink, tempC)
+        }else if(i === 7){
+            (tempC)
+            weatherSequenceDom_7(specificTimeSliced, specificWeatherIconLink, tempC)
         }
     }
+}
+
+
+
+
+const apiKey = "023ddb3cfa25160b6c95e5c0c0995ea0"
+
+searchIcon.addEventListener("click" , ()=>{
+
+    let cityName = input.value
     if(input.value ===""){
         ""
     }
@@ -117,10 +143,12 @@ searchIcon.addEventListener("click" , ()=>{
         getWeather(cityName, apiKey)
     }
 
+
 })
 
 
 
+window.onload = getWeather("London" , apiKey)
 
 
 
@@ -144,10 +172,4 @@ searchIcon.addEventListener("click" , ()=>{
 
 
 
-
-
-
-
-
-
-// console.log(getWeather(cityName, apiKey))
+// (getWeather(cityName, apiKey))
