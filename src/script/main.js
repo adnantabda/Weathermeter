6f42aaed1e932c799a94bb5c56d117c47ce12cb5
Adnan Tahir <adnantabda@gmail.com>
2024-06-01 21:13:20 +0300

    Add Exception handling

--- src/script/main.js
+++ src/script/main.js
@@ -7,6 +7,9 @@ import TileLayer from 'ol/layer/Tile.js';
 import View from 'ol/View.js';
 
 
+let errorMessage = document.getElementById("error-message")
+
+
 new Map({
     target: 'map',
     layers: [
@@ -56,12 +59,36 @@ function todayHighlight(weatherDescription, weatherHumidity, weatherPressure, we
 }
 
 
+function convertUnixTimestamp(unixTimestamp) {
+    // Create a new JavaScript Date object based on the timestamp (multiplied by 1000 to convert from seconds to milliseconds)
+    const date = new Date(unixTimestamp * 1000);
+
+    // Format the date as desired
+    const hours = date.getHours();
+    const minutes = "0" + date.getMinutes();
+    const seconds = "0" + date.getSeconds();
+
+    // Will display time in 24-hour format: hh:mm:ss
+    const formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+    return formattedTime;
+}
+
 
 async function getLocation(cityName, apiKey){
-    const latLongApi= `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`
-    const response = await fetch(latLongApi , {mode:'cors'})
-    const data = await response.json()
-    return  [data[0]["lat"], data[0]["lon"]]
+    try{
+        const latLongApi= `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`
+        const response = await fetch(latLongApi , {mode:'cors'})
+        const data = await response.json()
+        return  [data[0]["lat"], data[0]["lon"]]
+
+    }catch{
+        errorMessage.style.display = "block"
+        setTimeout(() => {
+            errorMessage.style.display = "none"
+            
+        }, 3000)
+        }
 }
 
 
@@ -69,9 +96,16 @@ async function getWeather(cityName, apiKey){
     const location = await getLocation(cityName, apiKey)
    
 
-    getNearbyCities(apiKey,location[0], location[1], cityName)
+    try{
+        getNearbyCities(apiKey,location[0], location[1], cityName)
 
+    }catch{
+        ""
+    }
 
+    try{
+
+    
 
     const weatherApi = `https://api.openweathermap.org/data/2.5/forecast?lat=${location[0]}&lon=${location[1]}&appid=023ddb3cfa25160b6c95e5c0c0995ea0&cnt=8`
     const response = await fetch(weatherApi, {mode: 'cors'})
@@ -91,8 +125,8 @@ async function getWeather(cityName, apiKey){
     let weatherPressure = data["list"][0]['main']["pressure"]
     let weatherHumidity = data["list"][0]["main"]["humidity"]
     let weatherSealevel = data["list"][0]["main"]["sea_level"]
-    let weatherSunrise = data["city"]["sunrise"]
-    let weatherSunset = data["city"]["sunset"]
+    let weatherSunrise = convertUnixTimestamp(data["city"]["sunrise"])
+    let weatherSunset = convertUnixTimestamp(data["city"]["sunset"])
 
     todayHighlight(weatherDescription, weatherHumidity,weatherPressure , weatherSealevel, weatherSunset, weatherSunrise)
 
@@ -125,6 +159,9 @@ async function getWeather(cityName, apiKey){
             weatherSequenceDom_7(specificTimeSliced, specificWeatherIconLink, tempC)
         }
     }
+    }catch{
+    ""
+     }
 }
 
 
